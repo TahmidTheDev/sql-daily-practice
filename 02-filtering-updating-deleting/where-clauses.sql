10c69356d11a0dfdcdf4648c1082ad90447c7fc0
TahmidTheDev <tahmidwhafi@gmail.com>
2026-08-19 12:38:23 +0600

    Update where-clauses.sql

--- 02-filtering-updating-deleting/where-clauses.sql
+++ 02-filtering-updating-deleting/where-clauses.sql
@@ -1,1 +1,31 @@
 
+
+WHERE district = 'Dhaka';
+
+
+SELECT * FROM customers
+WHERE total_purchase > 3000;
+
+
+SELECT * FROM customers
+WHERE district = 'Dhaka' AND total_purchase > 2000;
+
+
+SELECT * FROM customers
+WHERE district = 'Sylhet' OR district = 'Rajshahi';
+
+
+SELECT * FROM customers
+WHERE district IN ('Dhaka', 'Chattogram', 'Sylhet');
+
+
+SELECT * FROM customers
+WHERE district NOT IN ('Dhaka');
+
+
+SELECT * FROM customers
+WHERE total_purchase * 1.15 > 5000;
+
+
+SELECT * FROM customers
+WHERE total_purchase BETWEEN 2000 AND 5000;
